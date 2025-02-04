cc6dc92bee24fdaaa9df5c1aa95604d0efaefd14
Sayar Samanta <sayarsamanta@gmail.com>
2025-02-04 11:52:01 +0530

    Delete src/utils directory

--- src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-export function validEmail(email) {
-  const isValid = !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email);
-  return isValid;
-}
-
-export function validPassword(password) {
-  var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  return re.test(password);
-}
